07558560a5f2eaf77bf636c9513f7c2bb688e2fa
ozihler <D%-N7cCB32qjivkd>
2020-04-03 18:13:37 +0200

    remove frontend

--- frontend/src/app/index/welcome-message.ts
+++
@@ -1,3 +0,0 @@
-export interface WelcomeMessage {
-  welcomeMessage: string;
-}
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  eventName: "covid19hackaton",
-  production: true,
-  baseUrl: `${location.origin}/api`
-};
