7c353ffa57282a21399ff097f619a00110cc28a6
ozihler <ozihler@gmail.com>
2020-04-04 15:45:18 +0200

    add fields

--- backend/src/main/java/com/hackaton/covid19/register/adapters/presentation/viewmodels/PandeBuddyJson.java
+++ backend/src/main/java/com/hackaton/covid19/register/adapters/presentation/viewmodels/PandeBuddyJson.java
@@ -2,9 +2,16 @@ package com.hackaton.covid19.register.adapters.presentation.viewmodels;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class PandeBuddyJson {
     @JsonProperty("pandeName")
     private String username;
+
+    private List<PandeBuddyJson> pandeBuddies;
+    private String imageUrl;
+    private ScoreJson score = new ScoreJson();
+
 
     public PandeBuddyJson() {
     }
@@ -15,5 +22,33 @@ public class PandeBuddyJson {
 
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<PandeBuddyJson> getPandeBuddies() {
+        return pandeBuddies;
+    }
+
+    public void setPandeBuddies(List<PandeBuddyJson> pandeBuddies) {
+        this.pandeBuddies = pandeBuddies;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public ScoreJson getScore() {
+        return score;
+    }
+
+    public void setScore(ScoreJson score) {
+        this.score = score;
     }
 }
