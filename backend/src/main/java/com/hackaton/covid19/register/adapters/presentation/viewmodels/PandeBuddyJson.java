7d82d79881a44e87df9f59ab16324cbd123998af
ozihler <ozihler@gmail.com>
2020-04-04 15:38:03 +0200

    rename

--- backend/src/main/java/com/hackaton/covid19/register/adapters/presentation/viewmodels/PandeBuddyJson.java
+++ backend/src/main/java/com/hackaton/covid19/register/adapters/presentation/viewmodels/PandeBuddyJson.java
@@ -1,6 +1,9 @@
 package com.hackaton.covid19.register.adapters.presentation.viewmodels;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PandeBuddyJson {
+    @JsonProperty("pandeName")
     private String username;
 
     public PandeBuddyJson() {
