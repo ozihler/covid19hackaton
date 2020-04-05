f5961ed63fc1eb9ff96de2d762c114932ccbb571
ozihler <ozihler@gmail.com>
2020-04-05 12:22:58 +0200

    add get score

--- backend/src/main/java/com/hackaton/covid19/dailyquestionnaire/DailyQuestionnaireService.java
+++ backend/src/main/java/com/hackaton/covid19/dailyquestionnaire/DailyQuestionnaireService.java
@@ -3,7 +3,9 @@ package com.hackaton.covid19.dailyquestionnaire;
 import com.hackaton.covid19.register.adapters.presentation.viewmodels.ScoreJson;
 import com.hackaton.covid19.shared.adapters.data_access.InMemoryPandeBuddyRepository;
 import com.hackaton.covid19.shared.adapters.data_access.exceptions.PandeBuddyNotFoundException;
+import com.hackaton.covid19.shared.domain.entities.PandeBuddy;
 import com.hackaton.covid19.shared.domain.values.Username;
+import org.h2.engine.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +31,15 @@ public class DailyQuestionnaireService {
         dailyQuestionnaires.add(dailyQuestionnaire);
         CalculatedScoreDaily calculatedScore = new CalculatedScoreDaily(repository.withUsername(username).get());
         return calculatedScore.value();
+    }
+
+    public ScoreJson getScoreFor(String pandeName) {
+        Username username = Username.from(pandeName);
+        if (!repository.exists(username)) {
+            throw new PandeBuddyNotFoundException(username);
+        }
+
+        PandeBuddy pandeBuddy = repository.withUsername(username).get();
+        return pandeBuddy.getScore();
     }
 }
--- backend/src/main/java/com/hackaton/covid19/dailyquestionnaire/SubmitDailyQuestionnaireResource.java
+++ backend/src/main/java/com/hackaton/covid19/dailyquestionnaire/SubmitDailyQuestionnaireResource.java
@@ -2,6 +2,8 @@ package com.hackaton.covid19.dailyquestionnaire;
 
 import com.hackaton.covid19.entryquestionnaire.EntryQuestionnaireService;
 import com.hackaton.covid19.register.adapters.presentation.viewmodels.ScoreJson;
+import com.hackaton.covid19.shared.adapters.data_access.InMemoryPandeBuddyRepository;
+import com.hackaton.covid19.shared.domain.values.Score;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,15 +13,24 @@ import org.springframework.web.bind.annotation.*;
 public class SubmitDailyQuestionnaireResource {
 
     private DailyQuestionnaireService dailyQuestionnaireService;
+    private InMemoryPandeBuddyRepository repository;
 
     @Autowired
-    public SubmitDailyQuestionnaireResource(DailyQuestionnaireService dailyQuestionnaireService) {
+    public SubmitDailyQuestionnaireResource(DailyQuestionnaireService dailyQuestionnaireService ) {
         this.dailyQuestionnaireService = dailyQuestionnaireService;
+        this.repository = repository;
     }
 
     @PostMapping("dailyQuestionnaire")
     public ResponseEntity<ScoreJson> submitDailyQuestionnaire(@RequestHeader("pandeName") String pandeName, @RequestBody DailyQuestionnaire dailyQuestionnaire) {
-        ScoreJson score = this.dailyQuestionnaireService.storeQuestionnaire(pandeName,dailyQuestionnaire);
+        ScoreJson score = this.dailyQuestionnaireService.storeQuestionnaire(pandeName, dailyQuestionnaire);
         return ResponseEntity.ok(score);
     }
+
+    @GetMapping("curentScore")
+    public ResponseEntity<ScoreJson> getCurrentScore(@RequestHeader("pandeName") String pandeName) {
+        ScoreJson score = dailyQuestionnaireService.getScoreFor(pandeName);
+        return ResponseEntity.ok(score);
+    }
+
 }
